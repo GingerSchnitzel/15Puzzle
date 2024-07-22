f015d376bbe34ca0fc58ce5df4a0aeda82bf283e
GingerSchnitzel <141836349+GingerSchnitzel@users.noreply.github.com>
2024-07-22 13:40:08 +0300

    Created separate Tile and Board .cpp and .h files

---
+++ 15Puzzle/Main.cpp
@@ -0,0 +1,28 @@
+/*
+* CLASS BOARD
+* Display the game board
+* Randomize starting state
+* Slide tiles
+* Determine if win condition is reached
+* 
+* CLASS TILE
+* Display an individual tile
+* 
+* NAMESPACE USERINPUT
+* Get commands from user
+* Handle invalid input
+* 
+* FUNCTION MAIN
+* Main game logic loop
+* Allow user to quit before winning
+* 
+* HELPER CLASSES
+* 1. CLASS POINT
+* Indexing the game board tiles (x-axis and y-axis coordinatees)
+* 
+* 2. CLASS DIRECTION
+* Make working with directional commmands easier and more intuitive (enum direction)
+
+
+
+*/
